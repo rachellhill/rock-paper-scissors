4ac6d418e1ce3333734271f8217851516389268f
Rachel Hill <hillrachel187@gmail.com>
2022-03-04 13:01:19 -0700

    Create game functionality foundation

---
+++ game.js
@@ -0,0 +1,43 @@
+//two player instances ✅
+// A way to keep track of the data for the game board
+  // what's the data?
+    // player1's choice, player 2's choice, winner
+// A way to keep track of the selected game type
+  // somehow will need to differentiate classic vs difficult
+// A way to check the Game’s board data for win conditions
+  // create a new property of Game (this.winConditions) for both classic and difficult
+  // can use an array of strings comparing each
+// A way to detect when a game is a draw (no one has won)
+// A way to reset the Game’s board to begin a new game
+
+class Game {
+  constructor() {
+    this.player1 = new Player('Human', '👩');
+    this.player2 = new Player('Computer', '💻');
+    this.winner = null;
+    this.winsConditionsClassic = ['rock > scissors', 'scissors > paper', 'paper > rock'];
+  }
+  playGame() {
+    this.player1.takeTurn(this.player1.classicChoices);
+    console.log(this.player1.fighter);
+    this.player2.getComputerChoice(this.player2.classicChoices);
+    console.log(this.player2.fighter);
+  }
+  // if user clicks on one of the picture icons, that data is stored here and a new instance of this class is in the playGame function to check the users choice against the computer's randomly generated choice
+  // if player1's fighter is strictly equal to player2's fighter, return string "its a draw!"
+  // if player1's fighter is > player 2s fighter, return player 1 as winner
+    // check against wins condition here
+  // if player2's
+  checkWinner() {
+    if (this.player1.fighter === this.player2.fighter) {
+      return "it's a draw!"
+    } else if (this.winsConditionsClassic.includes(`${this.player1.fighter} > ${this.player2.fighter}`)) {
+      this.winner = this.player1.name;
+      this.player1.wins++;
+      return `${this.player1.name} won!`
+    } else {
+      this.player2.wins++;
+      return `${this.player2.name} won!`
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,38 @@
+// var game = new Game();
+var classicButton = document.querySelector(".classic-game-button");
+var difficultButton = document.querySelector(".difficult-game-button");
+
+
+// functions 👇
+// make the game playable without going through the DOM
+// compare what is selected to the rules
+// computer's choice and user choice
+// compare the two choices based on RPS rules and return winner or draw
+// generate rock, paper, scissors
+
+// generate random choice for Computer
+// iterate through classicChoices and return a choice
+
+// compare the choice to the human's choice
+  // if human choice ===
+
+// rock paper scissors diagram
+  // scissors beats paper, paper beats rock, rock beats scissors ...
+  // choice is pushed to the middle of the array,
+  // maybe it checks to see what index it is in the array'
+
+  // how do I know what human's choice is?
+
+// maybe should I have a function for a win, draw and loss?
+  // ex: function win()
+  // if
+
+
+
+
+
+// function playGame() {
+//   if () {
+//
+//   }
+// }
